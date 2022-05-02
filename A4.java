205c2e2be57b004314bad8e1ff64268d231d17dd
lavish19 <66295129+lavish19@users.noreply.github.com>
2022-05-02 10:22:16 +0530

    Add files via upload

---
+++ A4.java
@@ -0,0 +1,42 @@
+package com.abstraction; 
+abstract class MyTest 
+{ 
+  abstract void calculate(int a, int b); // No body. 
+ } 
+ class Addition extends MyTest 
+{ 
+  void calculate(int a, int b)
+  { 
+    int x = a + b; 
+    System.out.println("Sum:"  +x); 
+   } 
+} 
+ class Subtraction extends MyTest 
+{ 
+  void calculate(int a, int b)
+  { 
+    int y = a - b; 
+    System.out.println("Subtract:"  +y); 
+  } 
+} 
+ class Multiplication extends MyTest 
+{ 
+  void calculate(int a, int b)
+  { 
+    int z = a * b; 
+    System.out.println("Multiply:"  +z); 
+  } 
+} 
+public class A4
+{ 
+ public static void main(String[] args) 
+ { 
+   Addition a = new Addition(); 
+   Subtraction s = new Subtraction(); 
+   Multiplication m = new Multiplication(); 
+    
+    a.calculate(20, 30); 
+    s.calculate(10, 5); 
+    m.calculate(10, 20); 
+  } 
+}
