8f6f27be969ed5e2386e4ae781f0d5f5678b3b69
lavish19 <66295129+lavish19@users.noreply.github.com>
2022-05-06 10:57:24 +0530

    Add files via upload

---
+++ St1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class St1 {
+	public static void main(String[] args) {
+		Stack<Integer> s=new Stack<Integer>();
+		s.push(9);
+		s.push(10);
+		s.push(99);
+		s.push(0);
+		s.push(02);
+		System.out.println("is stack is empty "+s.empty());
+		System.out.println("elements of stack is "+ s);
+		
+	}
+
+}
